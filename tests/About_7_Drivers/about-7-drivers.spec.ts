4fc45bf8334d8b9e6cf47bfb235c87939d9f3b29
Meenakshi sundaram S <meenakshisundarams23@gmail.com>
2026-06-13 13:54:18 +0530

    NEw modification on 13June

--- tests/About_7_Drivers/about-7-drivers.spec.ts
+++ tests/About_7_Drivers/about-7-drivers.spec.ts
@@ -4,50 +4,7 @@
 import { test, expect, type Page, type Locator } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 import { DashboardPage } from '../../pages/DashboardPage';
-
-class AboutSevenDriversPage {
-  readonly focusMenu: Locator;
-  readonly aboutSevenDriversLink: Locator;
-  readonly nextButton: Locator;
-  readonly pageTitle: Locator;
-
-  constructor(private page: Page) {
-    this.focusMenu = page.locator('a.dropdown-toggle.nav-link', { hasText: 'Focus' });
-    this.aboutSevenDriversLink = page.locator("(//span[normalize-space(text())='About 7 Drivers'])[1]");
-    this.nextButton = page.locator('button', { hasText: 'Next' });
-    this.pageTitle = page.getByText('EY 7 Drivers of Growth').nth(0);
-  }
-
-  async clickFocusMenu() {
-    await this.focusMenu.click();
-  }
-
-  async clickAboutSevenDriversSubmenu() {
-    await this.aboutSevenDriversLink.click();
-  }
-
-  async expectAboutPageLoaded() {
-    await expect(this.pageTitle).toBeVisible();
-    await expect(this.page).toHaveURL(/\/focus\/about-seven-drivers$/);
-  }
-
-  async clickNext() {
-    await this.nextButton.click();
-    await expect(this.page).toHaveURL(/\/focus\/about-seven-drivers\/steps/);
-  }
-
-  async expectStepLabelVisible(step: string) {
-    await expect(this.page.locator('div.step-title', { hasText: step })).toBeVisible();
-  }
-
-  async clickStepTitle(step: string) {
-    await this.page.locator('div.step-title', { hasText: step }).click();
-  }
-
-  async expectStepTitleActive(step: string) {
-    await expect(this.page.locator('div.step-title.active', { hasText: step })).toBeVisible();
-  }
-}
+import { AboutSevenDriversPage } from '../../pages/AboutsevendriversPage';
 
 test.describe('About 7 Drivers Flow', () => {
   test('Navigate to About 7 Drivers and verify step navigation', async ({ page }) => {
